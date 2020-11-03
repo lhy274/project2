4f700d82c903de8a99e6d0620b20737f9ad5e85a
eunjoo <ejoo1109@nate.com>
2020-11-03 17:18:42 +0900

    수정하였습니다.

--- src/project2/Test.java
+++ src/project2/Test.java
@@ -6,7 +6,8 @@ public class Test {
 		// TODO Auto-generated method stub
 		System.out.println("안녕하세요");
 		System.out.println("반갑습니다.");
-		Sysout.out.prinf("안녕");
+		System.out.println("안녕");
+		System.out.println("Hello~java");
 	}
 
 }
