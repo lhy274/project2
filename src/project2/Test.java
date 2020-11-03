c619fe6606344c5ecb62d0411c923f61b4f49fb6
hyunyoung <lhy76927@gmail.com>
2020-11-03 17:18:43 +0900

    충동

--- src/project2/Test.java
+++ src/project2/Test.java
@@ -1,11 +1,30 @@
 package project2;
 
+import java.util.ArrayList;
+
 public class Test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("안녕하세요");
 		System.out.println("반갑습니다.");
+		System.out.println("Hello");
+		System.out.println("안녕하세요");
+		System.out.println("반갑습니다.");
+		System.out.println("Hello");
+		ArrayList[] list = new ArrayList[5];
+		System.out.println("안녕하세요");
+		System.out.println("반갑습니다.");
+		System.out.println("Hello");
+		System.out.println("안녕하세요");
+		System.out.println("반갑습니다.");
+		System.out.println("Hello");
+		System.out.println("안녕하세요");
+		System.out.println("반갑습니다.");
+		System.out.println("Hello");
+		System.out.println("안녕하세요");
+		System.out.println("반갑습니다.");
+		System.out.println("Hello");
 	}
 
 }
